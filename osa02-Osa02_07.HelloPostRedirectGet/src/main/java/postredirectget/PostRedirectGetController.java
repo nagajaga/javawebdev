0cbbb641d0d221fd0929439c4ff75d672cdb9629
Joel Wang <workworkjoel@gmail.com>
2023-06-29 11:01:32 +0300

    p2e7

--- osa02-Osa02_07.HelloPostRedirectGet/src/main/java/postredirectget/PostRedirectGetController.java
+++ osa02-Osa02_07.HelloPostRedirectGet/src/main/java/postredirectget/PostRedirectGetController.java
@@ -17,4 +17,16 @@ public class PostRedirectGetController {
         this.list = new ArrayList<>();
     }
 
+    @GetMapping("/")
+    public String home(Model model) {
+        model.addAttribute("list", list);
+        return "index";
+    }
+
+    @PostMapping("/")
+    public String post(@RequestParam String data) {
+        this.list.add(data);
+        return "redirect:/";
+    }
+
 }
