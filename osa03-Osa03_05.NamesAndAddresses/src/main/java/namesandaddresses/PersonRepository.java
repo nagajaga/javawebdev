6beb3f7bd5d2c61143068c3d0973cae47f7be786
Joel Wang <workworkjoel@gmail.com>
2023-07-05 13:41:23 +0300

    3.5

--- osa03-Osa03_05.NamesAndAddresses/src/main/java/namesandaddresses/PersonRepository.java
+++ osa03-Osa03_05.NamesAndAddresses/src/main/java/namesandaddresses/PersonRepository.java
@@ -1,7 +1,14 @@
 package namesandaddresses;
 
+import java.util.List;
+
+import javax.persistence.Entity;
+
+
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PersonRepository extends JpaRepository<Person, Long> {
-
+    @EntityGraph(value = "Person.address")
+    List<Person> findAll();
 }
