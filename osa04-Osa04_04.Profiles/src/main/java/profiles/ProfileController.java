1165eceebc373ca778178ce9830a5319b15a8547
Joel Wang <workworkjoel@gmail.com>
2023-07-11 20:15:52 +0300

    4.4

--- osa04-Osa04_04.Profiles/src/main/java/profiles/ProfileController.java
+++ osa04-Osa04_04.Profiles/src/main/java/profiles/ProfileController.java
@@ -1,21 +1,28 @@
 package profiles;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class ProfileController {
 
+    @Autowired
+    Environment env;
 
     @ResponseBody
     @GetMapping("/profile")
     public String getProfile() {
-        return "Hello world!";
+        return env.getActiveProfiles()[0];
     }
 
-    public String home() {
+    @GetMapping("/")
+    public String home(Model model) {
+        model.addAttribute("currentProfile", env.getActiveProfiles()[0]);
         return "index";
     }
 
